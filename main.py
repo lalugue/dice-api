811c916436f05dd250b8d6f02de22808ebf9045a
lalugue <51893731+lalugue@users.noreply.github.com>
2022-05-19 13:37:35 +0800

    add values and dice generation

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from typing import Union
-
 from fastapi import FastAPI
+from random import randrange
 
 app = FastAPI()
 
+valid_dice_values = ['d4','d6','d8','d10','d12','d20']
+
+def generate_dice_value(dice):
+    if dice not in valid_dice_values:
+        return "invalid"
+    sides = int(dice[1:])
+    return randrange(1,sides+1)
 
 @app.get("/")
 def read_root():
