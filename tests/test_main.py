c6474fae20cfaf60724df3aa7c1ee79fab8b2f0e
lalugue <51893731+lalugue@users.noreply.github.com>
2022-08-20 16:52:56 +0800

    shorten main testing method name

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,7 +5,7 @@ from main import app
 client = TestClient(app)
 
 
-def test_read_main():
+def test_main():
     response = client.get("/")
     assert response.status_code == 200
     assert "id" and "dice" and "value" and "links" in response.json()
