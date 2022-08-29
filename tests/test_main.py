313e51e4887606bcb0f188ead22a5f3cd96c3f43
lalugue <51893731+lalugue@users.noreply.github.com>
2022-08-29 12:56:50 +0800

    update tests to check 422

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,12 +16,12 @@ def test_valid_dice():
 
 def test_invalid_dice():
     response = client.get("/dice/d4,d5,d8,d12,d20")
-    assert response.status_code == 400
+    assert response.status_code == 422
 
 def test_invalid_input():
     response = client.get("/dice/d4,d6,d8-d12,d20")
-    assert response.status_code == 400
+    assert response.status_code == 422
     response = client.get("/dice/d4,d6,")
-    assert response.status_code == 400
+    assert response.status_code == 422
     response = client.get("/dice/d4d6d8")
-    assert response.status_code == 400  
+    assert response.status_code == 422 
