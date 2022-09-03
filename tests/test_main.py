bb2d0646e47bcbb500e008fbc69c7ccbdc11b78a
lalugue <51893731+lalugue@users.noreply.github.com>
2022-09-03 10:22:18 +0800

    update tests with query parameters

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,17 +16,17 @@ def test_default_dice():
     assert "id" and "dice" and "value" and "links" in response.json()
 
 def test_valid_dice():
-    response = client.get("/dice/d4,d6,d8,d12,d20")
+    response = client.get("/dice/?dice=d4,d6,d8,d12,d20")
     assert response.status_code == 200
 
 def test_invalid_dice():
-    response = client.get("/dice/d4,d5,d8,d12,d20")
+    response = client.get("/dice/?dice=d4,d5,d8,d12,d20")
     assert response.status_code == 422
 
 def test_invalid_input():
-    response = client.get("/dice/d4,d6,d8-d12,d20")
+    response = client.get("/dice/?dice=d4,d6,d8-d12,d20")
     assert response.status_code == 422
-    response = client.get("/dice/d4,d6,")
+    response = client.get("/dice/?dice=d4,d6,")
     assert response.status_code == 422
-    response = client.get("/dice/d4d6d8")
+    response = client.get("/dice/?dice=d4d6d8")
     assert response.status_code == 422 
